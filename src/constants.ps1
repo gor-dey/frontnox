0424f5e9d6a1e7db1166999edd24ff70e8e2b0c6
gor-dey <gordeychepikov@gmail.com>
2026-08-01 01:45:59 +0500

    chore: bump version to 1.0.2

--- src/constants.ps1
+++ src/constants.ps1
@@ -1,10 +1,10 @@
-﻿# Copyright 2026 gor-dey
+# Copyright 2026 gor-dey
 # Licensed under the Apache License, Version 2.0 (the "License")
 # You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 # Unless required by applicable law or agreed to in writing, software distributed under the License is "AS IS" BASIS.
 
 # --- FrontNox Constants ---
-$NoxVersion     = "1.0.0"
+$NoxVersion     = "1.0.2"
 $NoxBaseDir     = Join-Path $HOME ".config\FrontNox"
 $NoxBinDir      = Join-Path $NoxBaseDir "bin"
 $NoxConfigDir   = $NoxBaseDir
